f614eb96a72cc3800aa54bb69b88f23ec4ac3ca9
Adam Whitehouse <adswhitehouse95@gmail.com>
2025-03-02 13:47:01 +0000

    form can be submitted

---
+++ script.js
@@ -0,0 +1,19 @@
+let emailInput = document.querySelector(".jsEmailInput");
+let message = document.querySelector(".jsMessage");
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycby6BEDcVeeZJXvAS4eu1R3R6_MPrFOO7NEVSICSjzfmSyYN62cmNAx-cKhGuR6ep4MM/exec";
+const form = document.forms["submit-to-google-sheet"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      message.textContent = "Thank You For Your Submission";
+      form.reset()
+      setTimeout(() => {
+        message.textContent = ""
+      }, 5000);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
